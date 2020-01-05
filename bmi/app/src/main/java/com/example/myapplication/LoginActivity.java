eda449ccc01641f5d08bbb4baac60a87f72d415a
kaihua-li <58722365+kaihua-li@users.noreply.github.com>
2020-01-05 17:09:57 +0800

    updata login

--- bmi/app/src/main/java/com/example/myapplication/LoginActivity.java
+++ bmi/app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -1,26 +1,41 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText emailId;
+    EditText password;
+    Button bLogin;
+    TextView registerLink;
+   FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        final  EditText etUsername = (android.widget.EditText) findViewById(R.id.etUsername);
-        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
-        final Button bLogin = (Button) findViewById(R.id.bLogin);
-        final TextView registerLink = (TextView) findViewById(R.id.tvRegisterHere);
+        mFirebaseAuth=FirebaseAuth.getInstance();
 
+        emailId = (android.widget.EditText) findViewById(R.id.etEmail);
+        password = (EditText) findViewById(R.id.etPassword);
+        bLogin = (Button) findViewById(R.id.bLogin);
+        registerLink = (TextView) findViewById(R.id.tvRegisterHere);
 
 
         registerLink.setOnClickListener(new View.OnClickListener() {
@@ -34,11 +49,39 @@ public class LoginActivity extends AppCompatActivity {
         bLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                Intent LoginIntent = new Intent(LoginActivity.this,UserAreaActivity.class);
-                LoginActivity.this.startActivity(LoginIntent);
+                String email =emailId.getText().toString().trim();
+                String pwd= password.getText().toString().trim();
+                if(email.isEmpty()){
+                    emailId.setError("請輸入信箱");
+                    emailId.requestFocus();
+                }
+                else if(pwd.isEmpty()){
+                    password.setError("請輸入密碼");
+                    password.requestFocus();
+                }
+                else if(email.isEmpty() && pwd.isEmpty()){
+                    Toast.makeText(LoginActivity.this,"Fields are Empty!",Toast.LENGTH_SHORT);
+                }
+                else if(!(email.isEmpty() && pwd.isEmpty())){
+                    mFirebaseAuth.createUserWithEmailAndPassword(email,pwd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(LoginActivity.this,"Signup Unsuccessful,Please try again!!!",Toast.LENGTH_SHORT);
+                            }
+                            else{
+                                startActivity(new Intent(LoginActivity.this,UserAreaActivity.class));
+                            }
+
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(LoginActivity.this,"Error Occurred!!!",Toast.LENGTH_SHORT);
+                }
+
             }
 
         });
-
     }
 }
